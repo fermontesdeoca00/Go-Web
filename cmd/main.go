15eb0ffab216802d08f3abdf243fdae40e4a792c
Fernando <fernando.montesdeoca@mercadolibre.com>
2023-09-10 23:52:46 -0300

    feat: main implementation with repository, service and handler. use of gin with get and post methods

--- cmd/main.go
+++ cmd/main.go
@@ -1,42 +1,49 @@
 package main
 
 import (
+	"encoding/json"
+	"os"
+
+	"github.com/fermontesdeoca00/Go-Web/cmd/server/handler"
+	"github.com/fermontesdeoca00/Go-Web/internal/domain"
+	"github.com/fermontesdeoca00/Go-Web/internal/product"
 	"github.com/gin-gonic/gin"
 )
 
 var nextID int
 var Token = "123456"
 
-type Product struct {
-	ID           int     `json:"id,omitempty"`
-	Name         string  `json:"name,omitempty"`
-	Quantity     int     `json:"quantity,omitempty"`
-	Code_Value   string  `json:"code_value,omitempty"`
-	IS_Published bool    `json:"is_published"`
-	Expiration   string  `json:"expiration,omitempty"`
-	Price        float64 `json:"price,omitempty"`
-}
-
-type Request struct {
-	ID           int     `json:"id,omitempty"`
-	Name         string  `json:"name,omitempty"`
-	Quantity     int     `json:"quantity,omitempty"`
-	Code_Value   string  `json:"code_value,omitempty"`
-	IS_Published bool    `json:"is_published"`
-	Expiration   string  `json:"expiration,omitempty"`
-	Price        float64 `json:"price,omitempty"`
-}
-
 func main() {
 
-	server := gin.Default()
+	var productsList []domain.Product
+	loadProducts("products.json", &productsList)
 
-	// server.GET("/ping", ping)
-	// server.GET("/products", allProducts)
-	// server.GET("/products/:id", searchById)
-	// server.GET("/products/search", getProductsByPrice)
-	// server.POST("/products", addProduct)
+	repo := product.NewRepository(productsList)
+	service := product.NewService(repo)
+	productHandler := handler.NewProductHandler(service)
+
+	server := gin.Default()
+	server.GET("ping", func(c *gin.Context) { c.String(200, "pong") })
+	products := server.Group("/products")
+	{
+		products.GET("/", productHandler.GetAll())
+		products.GET("/:id", productHandler.GetByID())
+		products.GET("/search", productHandler.SearchByPrice())
+		products.POST("/", productHandler.Post())
+	}
 
 	server.Run(":8080")
 
+}
+
+// load all the products from a json file
+func loadProducts(path string, list *[]domain.Product) {
+	file, err := os.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal([]byte(file), &list)
+	if err != nil {
+		panic(err)
+	}
 }
